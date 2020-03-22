5aa5be026d2fe2aefd2804477f9c4af3200d0fbb
panos-stavrianos <pstavrianosmail@gmail.com>
2020-03-22 15:04:38 +0200

    testing github actions

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 // styles
 import "assets/css/bootstrap.min.css";
 import "assets/scss/paper-kit.scss";
-
 
 import 'tui-editor/dist/tui-editor-contents.css';
 import 'highlight.js/styles/github.css';
