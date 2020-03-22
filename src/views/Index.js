72800f4f13c29de7e0e7d967a02c589842689e58
panos-stavrianos <pstavrianosmail@gmail.com>
2020-03-22 19:45:24 +0200

    testing actions

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,62 +1,43 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-
-// reactstrap components
-
 // core components
 import IndexNavbar from "../components/Navbars/IndexNavbar";
 import IndexHeader from "./../components/Headers/IndexHeader.js";
 import IndexFooter from "./../components/Footers/IndexFooter.js";
 
+// reactstrap components
+
 // index sections
 
 function Index() {
-  document.documentElement.classList.remove("nav-open");
-  React.useEffect(() => {
-    document.body.classList.add("index");
-    return function cleanup() {
-      document.body.classList.remove("index");
-    };
-  });
-  return (
-    <>
-      <IndexNavbar />
-      <IndexHeader />
-      {/*<div className="main">*/}
-      {/*  <SectionButtons />*/}
-      {/*  <SectionNavbars />*/}
-      {/*  <SectionNavigation />*/}
-      {/*  <SectionProgress />*/}
-      {/*  <SectionNotifications />*/}
-      {/*  <SectionTypography />*/}
-      {/*  <SectionJavaScript />*/}
-      {/*  <SectionCarousel />*/}
-      {/*  <SectionNucleoIcons />*/}
-      {/*  <SectionDark />*/}
-      {/*  <SectionLogin />*/}
-      {/*  <SectionExamples />*/}
-      {/*  <SectionDownload />*/}
-        <IndexFooter />
-      {/*</div>*/}
-    </>
-  );
+    document.documentElement.classList.remove("nav-open");
+    React.useEffect(() => {
+        document.body.classList.add("index");
+        return function cleanup() {
+            document.body.classList.remove("index");
+        };
+    });
+    return (
+        <>
+            <IndexNavbar/>
+            <IndexHeader/>
+            {/*<div className="main">*/}
+            {/*  <SectionButtons />*/}
+            {/*  <SectionNavbars />*/}
+            {/*  <SectionNavigation />*/}
+            {/*  <SectionProgress />*/}
+            {/*  <SectionNotifications />*/}
+            {/*  <SectionTypography />*/}
+            {/*  <SectionJavaScript />*/}
+            {/*  <SectionCarousel />*/}
+            {/*  <SectionNucleoIcons />*/}
+            {/*  <SectionDark />*/}
+            {/*  <SectionLogin />*/}
+            {/*  <SectionExamples />*/}
+            {/*  <SectionDownload />*/}
+            <IndexFooter/>
+            {/*</div>*/}
+        </>
+    );
 }
 
 export default Index;
