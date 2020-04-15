6640f488ebf279b398c277060242b73f96c34c43
panos-stavrianos <pstavrianosmail@gmail.com>
2020-04-15 07:29:35 +0300

    adding SEO meta

--- src/index.js
+++ src/index.js
@@ -22,22 +22,7 @@ import Member from "./views/Member";
 import Show from "./views/Show";
 import Tag from "./views/Tag";
 //  others
-const default_meta = {
-    title: 'Ματζόρε FM 89,1',
-    description: 'Ο σταθμός του Πανεπιστημίου Κρήτης στο Ρέθυμνο',
-    meta: {
-        property: {
-            'og:title': 'Ματζόρε FM 89,1',
-            'og:description': 'Ο σταθμός του Πανεπιστημίου Κρήτης στο Ρέθυμνο',
-            'og:image': 'http://matzore.radio.uoc.gr/static/media/matzore_logo_192.f10c1636.png',
-            'og:type': 'music.radio_station',
-            'og:url':document.location.host
-        },
-        charset: 'utf-8',
-        name: {
-            keywords: 'react,meta,document,html,tags'
-        }
-    }}
+
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
