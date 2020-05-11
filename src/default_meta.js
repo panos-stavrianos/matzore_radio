2c3745f879e9c524e55b53d60beb65f04b82f004
panos-stavrianos <pstavrianosmail@gmail.com>
2020-05-11 14:18:27 +0300

    fix apache conf

--- src/default_meta.js
+++ src/default_meta.js
@@ -1,7 +1,7 @@
 export function get_default_meta({
                                      title = 'Ματζόρε FM 89,1',
                                      description = 'Ο σταθμός του Πανεπιστημίου Κρήτης στο Ρέθυμνο',
-                                     image = 'http://matzore.radio.uoc.gr/static/media/matzore_logo_192.f10c1636.png',
+                                     image = window.location.protocol+'//matzore.radio.uoc.gr/static/media/matzore_logo_192.f10c1636.png',
                                      type = 'music.radio_station',
                                      url = document.location.href,
                                      keywords = 'Ματζόρε,matzore,radio,radio station,rethymno,uoc,ρέθυμνο,φοιτητικός σταθμός,Πανεπιστημίου Κρήτης'
